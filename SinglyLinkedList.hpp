e18098d4f9056e64cf185c4f62511ac8b5080c2d
BENma15 <benma0215@gmail.com>
2025-10-18 16:45:09 +0000

    Finished Singly Linked List

--- SinglyLinkedList.hpp
+++ SinglyLinkedList.hpp
@@ -24,32 +24,117 @@ public:
     
     // Destructor
     ~SinglyLinkedList() {
-        // TODO: Delete all nodes to prevent memory leaks
+        Node* current = head;
+        while (current != nullptr) {
+            Node* next = current->next;
+            delete current;
+            current = next;
+        }
     }
     
     // TODO: Implement the operations (copy from lecture slides)
     T Head() const {
-        // TODO: Return the first element, throw exception if empty
+        if (head == nullptr) {
+            return 0;
+        }
+
+        return head->data;
     }
     
     T Tail() const {
-        // TODO: Return the last element, throw exception if empty
+        if (tail == nullptr) {
+            return 0;
+        }
+
+        return tail->data;
     }
     
     T FindKth(int k) const {
-        // TODO: Return the k-th element, throw exception if out of range
+        if (k < 0 || k >= size) {
+            return 0;
+        }
+
+        Node* current = head;
+        for (int i = 0; i < k; i++) {
+            current = current->next;
+        }
+
+        return current->data;
     }
     
     void Add(const T& value) {
-        // TODO: Add element to the end of the list
+        Node* newNode = new Node(value);
+
+        if (head == nullptr) {
+            head = tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+
+        size++;
     }
     
     void Insert(const T& value, int k) {
-        // TODO: Insert element at position k, throw exception if out of range
+        if (k < 0 || k > size) {
+            return;
+        }
+
+        Node* newNode = new Node(value);
+
+        Node* current = head;
+
+        if (k == 0) {
+            newNode->next = head;
+            head = newNode;
+            if (tail == nullptr) {
+                tail = newNode;
+            }
+        } else if (k == size) {
+            tail->next = newNode;
+            tail = newNode;
+        } else {
+            for (int i = 0; i < k - 1; i++) {
+                current = current->next;
+            }
+
+            newNode->next = current->next;
+            current->next = newNode;
+        }
+
+        size++;
     }
     
     void Remove(int k) {
-        // TODO: Remove element at position k, throw exception if out of range
+        if (k < 0 || k >= size) {
+            return;
+        }
+
+        Node* current = head;
+
+        if (k == 0) {
+            head = head->next;
+            if (head == nullptr) {
+                tail = nullptr;
+            }
+
+            delete current;
+        } else {
+            Node* prev = nullptr;
+            for (int i = 0; i < k; i++) {
+                prev = current;
+                current = current->next;
+            }
+
+            prev->next = current->next;
+            if (current == tail) {
+                tail = prev;
+            }
+
+            delete current;
+        }
+
+        size--;
     }
     
     // Utility functions
